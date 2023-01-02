4641efd5701ea171abad199a8007cab916f36c05
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2023-01-03 00:05:44 +0100

    chore: Write Sidebar component markup
    - Store nav lik details in a variable and export
    - Write markup and dynamically render nav links in Sidebar

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import sidebarData from "./sidebarData";
+import switchOrgIcon from "./assets/switch_organization.svg";
+import arrowIcon from "./assets/arrow_icon.svg";
+import dashboardIcon from "./assets/dashboard.svg";
+import sidebarStyles from "./sidebar.module.scss";
 
 const Sidebar = () => {
+  const { customers, business, settings } = sidebarData;
+
   return (
-    <div>Sidebar</div>
+    <div className={sidebarStyles.sidebar_container}>
+      <div className={sidebarStyles.switch_org_div}>
+        <img src={switchOrgIcon} alt="icon for switch organization" />
+        <p>Switch Organization</p>
+        <img src={arrowIcon} alt="arrow icon" />
+      </div>
+      <NavLink className={({ isActive }) => isActive ? sidebarStyles.active : ""} to="/">
+        <img src={dashboardIcon} alt="icon for dashboard" />
+        <p>Dashboard</p>
+      </NavLink>
+      <div>
+        <p>Customers</p>
+        {
+          customers.map((data, index) => (
+            <NavLink className={({ isActive }) => isActive ? sidebarStyles.active : ""} to={data.link} key={index}>
+              <img src={require(`./assets/${data.icon}.svg`)} alt={`icon for ${data.title}`} />
+              <p>{data.title}</p>
+            </NavLink>
+          ))
+        }
+      </div>
+      <div>
+        <p>Business</p>
+        {
+          business.map((data, index) => (
+            <NavLink className={({ isActive }) => isActive ? sidebarStyles.active : ""} to={data.link} key={index}>
+              <img src={require(`./assets/${data.icon}.svg`)} alt={`icon for ${data.title}`} />
+              <p>{data.title}</p>
+            </NavLink>
+          ))
+        }
+      </div>
+      <div>
+        <p>Settings</p>
+        {
+          settings.map((data, index) => (
+            <NavLink className={({ isActive }) => isActive ? sidebarStyles.active : ""} to={data.link} key={index}>
+              <img src={require(`./assets/${data.icon}.svg`)} alt={`icon for ${data.title}`} />
+              <p>{data.title}</p>
+            </NavLink>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/Sidebar/sidebarData.ts
@@ -0,0 +1,128 @@
+interface SidebarData {
+  customers: {
+      icon: string;
+      title: string;
+      link: string;
+  }[];
+  business: {
+      icon: string;
+      title: string;
+      link: string;
+  }[];
+  settings: {
+      icon: string;
+      title: string;
+      link: string;
+  }[];
+}
+
+const sidebarData: SidebarData = {
+  customers: [
+    {
+      icon: "users",
+      title: "Users",
+      link: "/users",
+    },
+    {
+      icon: "guarantors",
+      title: "Guarantors",
+      link: "/guarantors",
+    },
+    {
+      icon: "loans",
+      title: "Loans",
+      link: "/loans",
+    },
+    {
+      icon: "decision_models",
+      title: "Decision Models",
+      link: "/decision_models",
+    },
+    {
+      icon: "savings",
+      title: "Savings",
+      link: "/savings",
+    },
+    {
+      icon: "loan_requests",
+      title: "Loan Requests",
+      link: "/loan_requests",
+    },
+    {
+      icon: "whitelist",
+      title: "Whitelist",
+      link: "/whitelist",
+    },
+    {
+      icon: "karma",
+      title: "Karma",
+      link: "/karma",
+    },
+  ],
+  business: [
+    {
+      icon: "organization",
+      title: "Organization",
+      link: "/organization",
+    },
+    {
+      icon: "loan_products",
+      title: "Loan Products",
+      link: "/loan_products",
+    },
+    {
+      icon: "savings_products",
+      title: "Savings Products",
+      link: "/savings_products",
+    },
+    {
+      icon: "fees_and_charges",
+      title: "Fees and Charges",
+      link: "/fees_and_charges",
+    },
+    {
+      icon: "transactions",
+      title: "Transactions",
+      link: "/transactions",
+    },
+    {
+      icon: "services",
+      title: "Services",
+      link: "/services",
+    },
+    {
+      icon: "service_account",
+      title: "Service Account",
+      link: "/service_account",
+    },
+    {
+      icon: "settlements",
+      title: "Settlements",
+      link: "/settlements",
+    },
+    {
+      icon: "reports",
+      title: "Reports",
+      link: "/reports",
+    },
+  ],
+  settings: [
+    {
+      icon: "preferences",
+      title: "Preferences",
+      link: "/preferences",
+    },
+    {
+      icon: "fees_and_pricing",
+      title: "Fees and Pricing",
+      link: "/fees_and_pricing",
+    },
+    {
+      icon: "audit_logs",
+      title: "",
+      link: "/audit_logs",
+    },
+  ],
+}
+
+export default sidebarData;

d2be8ebdf59e12ad172028b076d6b3299650e7b4
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2023-01-10 04:36:46 +0100

    feat: Implement userDetails API call
    - Load user data from the API and store in localStorage
    - Fetch user data from localStorage and assign to state
    - Dynamically render userData in userDetails page, including
    generalDetailsTab component
    - Write styling adjustments
    - Import star icons to assets folder

--- src/pages/UserDetails/components/GeneralDetailsTab/index.tsx
+++ src/pages/UserDetails/components/GeneralDetailsTab/index.tsx
@@ -1,53 +1,58 @@
 import React from "react";
+import { useUser } from "../../index";
 import DetailCard from "./components/DetailCard";
 import generalDetailsTabStyles from "./generaldetailstab.module.scss";
 
 const GeneralDetailsTab = () => {
+  const { user } = useUser();
+
   return (
-    <div className={generalDetailsTabStyles.generaldetails_div}>
-      <div className={generalDetailsTabStyles.section_div}>
-        <h4>Personal Information</h4>
-        <div>
-          <DetailCard title="Full Name" detail="Grace Effiom" />
-          <DetailCard title="Phone Number" detail="07060780922" />
-          <DetailCard title="Email Address" detail="grace@gmail.com" />
-          <DetailCard title="BVN" detail="07060780922" />
-          <DetailCard title="Gender" detail="Female" />
-          <DetailCard title="Marital Status" detail="Single" />
-          <DetailCard title="Children" detail="None" />
-          <DetailCard title="Type of Residence" detail="Parent’s Apartment" />
+    user && (
+      <div className={generalDetailsTabStyles.generaldetails_div}>
+        <div className={generalDetailsTabStyles.section_div}>
+          <h4>Personal Information</h4>
+          <div>
+            <DetailCard title="Full Name" detail={`${user.profile.firstName} ${user.profile.lastName}`} />
+            <DetailCard title="Phone Number" detail={user.profile.phoneNumber} />
+            <DetailCard title="Email Address" detail={user.email.toLocaleLowerCase()} />
+            <DetailCard title="BVN" detail={user.profile.bvn} />
+            <DetailCard title="Gender" detail={user.profile.gender} />
+            <DetailCard title="Marital Status" detail="Single" />
+            <DetailCard title="Children" detail="None" />
+            <DetailCard title="Type of Residence" detail="Parent’s Apartment" />
+          </div>
+        </div>
+        <div className={generalDetailsTabStyles.section_div}>
+          <h4>Education and Employment</h4>
+          <div>
+            <DetailCard title="level of education" detail={user.education.level} />
+            <DetailCard title="employment status" detail={user.education.employmentStatus} />
+            <DetailCard title="sector of employment" detail={user.education.sector} />
+            <DetailCard title="Duration of employment" detail={user.education.duration} />
+            <DetailCard title="office email" detail={user.email.toLocaleLowerCase()} />
+            <DetailCard title="Monthly income" detail={`₦${user.education.monthlyIncome[0]}- ₦${user.education.monthlyIncome[0]}`} />
+            <DetailCard title="loan repayment" detail={user.education.loanRepayment} />
+          </div>
+        </div>
+        <div className={generalDetailsTabStyles.section_div}>
+          <h4>Socials</h4>
+          <div>
+            <DetailCard title="Twitter" detail={user.socials.twitter} />
+            <DetailCard title="Facebook" detail={user.socials.facebook} />
+            <DetailCard title="Instagram" detail={user.socials.instagram} />
+          </div>
+        </div>
+        <div className={generalDetailsTabStyles.section_div}>
+          <h4>Guarantor</h4>
+          <div>
+            <DetailCard title="full Name" detail={`${user.guarantor.firstName} ${user.guarantor.lastName}`} />
+            <DetailCard title="Phone Number" detail={user.guarantor.phoneNumber} />
+            <DetailCard title="Email Address" detail={user.email.toLocaleLowerCase()} />
+            <DetailCard title="Relationship" detail="Sister" />
+          </div>
         </div>
       </div>
-      <div className={generalDetailsTabStyles.section_div}>
-        <h4>Education and Employment</h4>
-        <div>
-          <DetailCard title="level of education" detail="B.Sc" />
-          <DetailCard title="employment status" detail="Employed" />
-          <DetailCard title="sector of employment" detail="FinTech" />
-          <DetailCard title="Duration of employment" detail="2 years" />
-          <DetailCard title="office email" detail="grace@lendsqr.com" />
-          <DetailCard title="Monthly income" detail="₦200,000.00- ₦400,000.00" />
-          <DetailCard title="loan repayment" detail="40,000" />
-        </div>
-      </div>
-      <div className={generalDetailsTabStyles.section_div}>
-        <h4>Socials</h4>
-        <div>
-          <DetailCard title="Twitter" detail="@grace_effiom" />
-          <DetailCard title="Facebook" detail="Grace Effiom" />
-          <DetailCard title="Instagram" detail="@grace_effiom" />
-        </div>
-      </div>
-      <div className={generalDetailsTabStyles.section_div}>
-        <h4>Guarantor</h4>
-        <div>
-          <DetailCard title="full Name" detail="Debby Ogana" />
-          <DetailCard title="Phone Number" detail="07060780922" />
-          <DetailCard title="Email Address" detail="debby@gmail.com" />
-          <DetailCard title="Relationship" detail="Sister" />
-        </div>
-      </div>
-    </div>
+    )
   )
 }
 

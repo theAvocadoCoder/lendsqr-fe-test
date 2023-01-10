f7a493eb81bb41557d1efd965f5cc2cc86a8e519
theAvocadoCoder <nwauwa.kelechi.e@gmail.com>
2023-01-10 12:12:26 +0100

    feat: Make nav components mobile responsive
    - Write logic for monitoring nav isOpen state
    - Write custom hook to monitor windowSize change
    - Refactore markup for dynamic mobile section
    - Write styles for mobile section
    - Import relevant pictures into respective assets folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { Dispatch, SetStateAction } from "react";
 import { Outlet } from "react-router-dom";
 import TopNav from "./components/TopNav";
 import Sidebar from "./components/Sidebar";
 import appStyles from "./app.module.scss";
 
+export type MobileSectionProps = {
+  mobileNavIsOpen: boolean,
+  setMobileNavIsOpen?: Dispatch<SetStateAction<boolean>>,
+}
+
 function App() {
+  const [mobileNavIsOpen, setMobileNavIsOpen] = React.useState(false);
   return (
     <div className="App">
       <div className={appStyles.app_container}>
         <div className={appStyles.top_nav}>
-          <TopNav />
+          <TopNav mobileNavIsOpen={mobileNavIsOpen} setMobileNavIsOpen={setMobileNavIsOpen} />
         </div>
         <div className={appStyles.sidebar}>
-          <Sidebar />
+          <Sidebar mobileNavIsOpen={mobileNavIsOpen} setMobileNavIsOpen={setMobileNavIsOpen} />
         </div>
         <div className={appStyles.outlet}>
           <Outlet />
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,57 +1,74 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import useWindowSize from "../hooks/useWindowSize";
+
 import sidebarData from "./sidebarData";
+
 import switchOrgIcon from "./assets/switch_organization.svg";
 import arrowIcon from "./assets/arrow_icon.svg";
 import dashboardIcon from "./assets/dashboard.svg";
-import sidebarStyles from "./sidebar.module.scss";
+import logoutIcon from "./assets/logout.svg";
 
-const Sidebar = () => {
+import sidebarStyles from "./sidebar.module.scss";
+import MobileSection from "../MobileSection";
+import type { MobileSectionProps } from "../../App";
+
+const Sidebar = ({ mobileNavIsOpen, setMobileNavIsOpen }: MobileSectionProps) => {
+  const windowSize = useWindowSize();
   const { customers, business, settings } = sidebarData;
 
+  React.useEffect(() => {
+    if (windowSize.innerWidth < 1024) {
+      if (setMobileNavIsOpen) setMobileNavIsOpen(false);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function toggleMobileNavIsOpen() {
+    if (setMobileNavIsOpen) setMobileNavIsOpen(!mobileNavIsOpen);
+  }
+
   return (
-    <div className={sidebarStyles.sidebar_container}>
+    <div className={
+      windowSize.innerWidth >= 1024
+        ? sidebarStyles.sidebar_container
+        : mobileNavIsOpen
+        ? `${sidebarStyles.sidebar_container} ${sidebarStyles.show}`
+        : sidebarStyles.hidden
+    }>
+      <div className={sidebarStyles.mobilesection_div}>
+        <MobileSection />
+      </div>
       <div className={sidebarStyles.switch_org_div}>
         <img src={switchOrgIcon} alt="icon for switch organization" />
         <p>Switch Organization</p>
         <img src={arrowIcon} alt="arrow icon" />
       </div>
-      <NavLink className={({ isActive }) => isActive ? sidebarStyles.active : ""} to="/">
+      <NavLink className={({ isActive }) => isActive ? sidebarStyles.active : ""} to="/" onClick={toggleMobileNavIsOpen}>
         <img src={dashboardIcon} alt="icon for dashboard" />
         <p>Dashboard</p>
       </NavLink>
-      <div>
-        <p>Customers</p>
-        {
-          customers.map((data, index) => (
-            <NavLink className={({ isActive }) => isActive ? sidebarStyles.active : ""} to={data.link} key={index}>
-              <img src={require(`./assets/${data.icon}.svg`)} alt={`icon for ${data.title}`} />
-              <p>{data.title}</p>
-            </NavLink>
-          ))
-        }
-      </div>
-      <div>
-        <p>Business</p>
-        {
-          business.map((data, index) => (
-            <NavLink className={({ isActive }) => isActive ? sidebarStyles.active : ""} to={data.link} key={index}>
-              <img src={require(`./assets/${data.icon}.svg`)} alt={`icon for ${data.title}`} />
-              <p>{data.title}</p>
-            </NavLink>
-          ))
-        }
-      </div>
-      <div>
-        <p>Settings</p>
-        {
-          settings.map((data, index) => (
-            <NavLink className={({ isActive }) => isActive ? sidebarStyles.active : ""} to={data.link} key={index}>
-              <img src={require(`./assets/${data.icon}.svg`)} alt={`icon for ${data.title}`} />
-              <p>{data.title}</p>
-            </NavLink>
-          ))
-        }
+      {
+        ["Customers", "Business", "Settings"].map((title, index) => (
+          <div key={index}>
+            <p>{title}</p>
+            {
+              [customers, business, settings][index].map((data, index) => (
+                <NavLink className={({ isActive }) => isActive ? sidebarStyles.active : ""} to={data.link} key={index} onClick={toggleMobileNavIsOpen}>
+                  <img src={require(`./assets/${data.icon}.svg`)} alt={`icon for ${data.title}`} />
+                  <p>{data.title}</p>
+                </NavLink>
+              ))
+            }
+          </div>
+        ))
+      }
+      <div className={sidebarStyles.footer_div}>
+        <NavLink className={({ isActive }) => isActive ? sidebarStyles.active : ""} to="/login">
+          <img src={logoutIcon} alt="icon for logout" />
+          <p>Logout</p>
+        </NavLink>
+        <span>v1.2.0</span>
       </div>
     </div>
   )
